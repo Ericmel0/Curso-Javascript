a978c541230f739f944f5707e1881cc7d34806cf
Eric Rian <eric.riaan@gmail.com>
2024-12-20 14:31:31 -0300

    estruturas de repetição

--- 2-Estrutura de dados/Variáveis/1exemplo/script.js
+++
@@ -1,7 +0,0 @@
-let nome = "Eric";
-var nome2 = "Rian";
-const nome3 = "Monteiro";
-console.log(nome);
-console.log(`meu nome é ${nome}`);
-console.log(nome2);
-console.log(nome3);
---
+++ 2-Estrutura de dados/estruturaDeRepetição/script.js
@@ -0,0 +1,6 @@
+let x = 10;
+
+while (x > 0){
+    console.log("O x é" + x);
+    x = x + 1;
+}
