b5710406a7935dfe6969aeba0f33bb1de1fae51a
Ericmel0 <eric.riaan@gmail.com>
2025-01-04 20:42:19 -0300

    funções

--- Curso-Javascript/1-Operadores e dados/1Exercício/Script.js
+++ Curso-Javascript/1-Operadores e dados/1Exercício/Script.js
@@ -1,3 +1,14 @@
-console.log("olá mundo")
-console.log('Olá mundo')
-console.log(`Olá mundo`)
+console.log("olá mundo");
+console.log("Olá mundo");
+console.log(`Olá mundo`);
+
+const a = 10
+const b = 15
+
+if(a > b){
+    console.log(`${a} é maior que ${b}`)
+    else{
+        console.log(`${b} é maior que ${a}`)
+        
+    }
+}
---
+++ Curso-Javascript/Funções/Introdução/script.js
@@ -0,0 +1,63 @@
+function inprimirNoConsole() {
+  console.log("olá mundo");
+}
+inprimirNoConsole();
+
+function inprimirUmNumero(num) {
+  console.log(`o numero é ${num}`);
+}
+inprimirUmNumero(2);
+
+const numeroAleatorio = function () {
+  console.log(Math.random());
+};
+
+numeroAleatorio();
+numeroAleatorio();
+numeroAleatorio();
+numeroAleatorio();
+
+/*mais sobre funções*/
+
+function multiplicarTresNumeros(x, y, z) {
+  return x * y * z;
+}
+
+/*quando colocamos o return, não basta apenas invocarmos a função,
+exemplo: multiplicarTresNumeros(2, 3, 10);
+deve-se utilizar o console.log antes
+console.log(multiplicarTresNumeros(2, 3, 4));
+*/
+
+console.log(multiplicarTresNumeros(2, 3, 4));
+
+/////////////////////////////////////////////
+
+/*Quando usamos uma variável o paramêtro dela fica fixo, quando não
+usamos colocamos os paramêtros após invocar ela */
+
+const mult = multiplicarTresNumeros(2, 4, 6);
+
+console.log(mult);
+
+/////////////////////////////////////////////
+
+/*dessa forma conseguimos "burlar" isso ao colocarmos a função
+na variável */
+
+const mut = function multiplicarDoisNumeros(a, b) {
+  return a * b;
+};
+
+console.log(mut(2, 8));
+
+/////////////////////////////////////////////
+
+/*novamente a forma de variável com paramêtro fixo */
+
+function multiDoisNumeros(a, b) {
+  return a * b;
+}
+const mit = multiDoisNumeros(2, 5);
+
+console.log(mit);
