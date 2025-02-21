a4458d66e7ada4390cbcc46cf054aea518e5d714
Eric Rian <eric.riaan@gmail.com>
2025-02-21 11:30:54 -0300

    objetos

--- Curso-Javascript/4 - Arrays e objetos/Métodos/scripts.js
+++ Curso-Javascript/4 - Arrays e objetos/Métodos/scripts.js
@@ -13,3 +13,39 @@ carro.ligar(); // "Carro ligado!"
 
 let corsa = { marca: "Toyota", ano: 2022 };
 console.log(corsa.marca); // "Toyota"
+
+/*Métodos de array
+
+Push adiciona, pop remove os último elemento da Array */
+
+let nomes = ["Eric", "Rick", "Matheus"];
+console.log(nomes);
+nomes.pop();
+console.log(nomes);
+nomes.push("Saulo");
+console.log(nomes);
+
+/*Shift remove, unshift adiciona o primeiro elemento da Array */
+
+let chinelo = ["havaiana"];
+console.log(chinelo);
+chinelo.unshift("kenner");
+console.log(chinelo);
+chinelo.shift();
+console.log(chinelo);
+
+/*IndexOf busca o primeiro elemento com o valor solicitado 
+e retorna sua posição, o lastindexof mas o mesmo com o último
+ elemento com o valor citado*/
+
+let numeros = [5, 4, 3, 9, 5, 3];
+console.log(numeros.indexOf(3));
+console.log(numeros.lastIndexOf(3));
+
+/*Slice retorna uma array com dados de outra array */
+
+let nu = [0, 1, 2, 3, 4, 5, 6, 7];
+console.log(nu.slice(0, 3));
+console.log(nu.slice(3));
+console.log(nu.slice(2, 5));
+console.log(nu.slice(-3));
