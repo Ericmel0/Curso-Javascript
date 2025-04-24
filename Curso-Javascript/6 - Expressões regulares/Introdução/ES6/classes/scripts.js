49c1ea8f68cc8a74b25fc009afdaf13ea5e391b7
Ericmel0 <eric.riaan@gmail.com>
2025-04-23 22:32:47 -0300

    regex2

--- Curso-Javascript/6 - Expressões regulares/Introdução/ES6/classes/scripts.js
+++ Curso-Javascript/6 - Expressões regulares/Introdução/ES6/classes/scripts.js
@@ -21,3 +21,25 @@ console.log(num.test("numero 61"));
 const num2 = /[0-9]/;
 console.log(num.test("numero 61"));
 console.log(num.test("numero 41424261"));
+
+//caracteres
+
+const pontoRegex = /./; //aceita tudo e retorna true
+const dRegex = /\d/; //caracteres númericos
+const DRegex2 = /\D/; //não aceita caracteres númericos
+const sRegex = /\s/; //aceita caracteres com espaços vazios
+const sRegex2 = /\S/; //não aceita caracteres com espaços vazios
+const wRegex = /\w/; // aceita caracteres alfanuméricos
+const wRegex2 = /\W/; // não aceita caracteres alfanuméricos
+
+let ano = /\d\d\d\d/;
+console.log(ano.test("05"));
+console.log(ano.test("2005"));
+
+let dias = /\d\d/;
+console.log(dias.test("05") && "05".length == 2);
+console.log(dias.test("2005") && "2005".length == 2);
+
+let palavraTresLetras = /\w\w\w/;
+console.log(palavraTresLetras.test("oi"));
+console.log(palavraTresLetras.test("olá"));
